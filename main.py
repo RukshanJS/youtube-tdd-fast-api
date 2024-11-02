6520f5e6926d040991ca1b6ac9a3f80eb94c91f2
RukshanJS <rukshanjspro@gmail.com>
2024-11-02 17:49:21 +0530

    feat: only create

--- main.py
+++ main.py
@@ -22,29 +22,7 @@ async def create_todo(todo: Todo):
     todos.append(todo)
     return todo
 
-@app.get("/todos/{todo_id}", response_model=Todo)
-async def read_todo(todo_id: int):
-    for todo in todos:
-        if todo.id == todo_id:
-            return todo
-    raise HTTPException(status_code=404, detail="Todo not found")
 
-@app.put("/todos/{todo_id}", response_model=Todo)
-async def update_todo(todo_id: int, updated_todo: Todo):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            updated_todo.id = todo_id
-            todos[index] = updated_todo
-            return updated_todo
-    raise HTTPException(status_code=404, detail="Todo not found")
-
-@app.delete("/todos/{todo_id}", status_code=204)
-async def delete_todo(todo_id: int):
-    for index, todo in enumerate(todos):
-        if todo.id == todo_id:
-            todos.pop(index)
-            return
-    raise HTTPException(status_code=404, detail="Todo not found")
 
 if __name__ == "__main__":
     import uvicorn
